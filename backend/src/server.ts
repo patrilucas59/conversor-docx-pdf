872838fc7b71a53396521373ac57b68ef20ad2b5
Lucas Patrício <patriciolucas059@gmail.com>
2026-02-26 22:50:55 -0300

    fix: adicionando rota da Vercel para o servidor

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -8,7 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://pdf-machine.vercel.app',
 }))
 
 app.use(express.json());
