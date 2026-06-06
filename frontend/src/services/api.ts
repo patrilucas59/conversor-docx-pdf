e938110259a5e0f937c03efb78d0174222c86566
Lucas Patrício <patriciolucas059@gmail.com>
2026-06-06 13:19:34 -0300

    feat: ajuste de tratamento de erros da API

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,16 +1,35 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 
 export const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
 })
 
 export async function convertDocxToPdf(file: File): Promise<Blob> {
-    const formData = new FormData();
-    formData.append('file', file);
+  const formData = new FormData();
+  formData.append("file", file);
 
+  try {
     const response = await api.post("/convert", formData, {
-        responseType: "blob",
+      responseType: "blob",
     });
 
     return response.data;
+    } catch (error: unknown) {
+    if (axios.isAxiosError(error)) {
+      const axiosError = error as AxiosError<Blob>;
+
+      if (axiosError.response?.data instanceof Blob) {
+        const text = await axiosError.response.data.text();
+
+        try {
+          const json = JSON.parse(text);
+          throw new Error(json.error);
+        } catch {
+          throw new Error(text);
+        }
+      }
+    }
+
+    throw error;
+  }
 }
